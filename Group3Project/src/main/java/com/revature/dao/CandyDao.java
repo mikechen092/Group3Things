4610a4da3cd375391603d6d384f9b0b702df4431
Omar Mohamad <omarbmohamad@gmail.com>
2018-02-22 12:50:20 -0500

    Group 3 Site
    Added Candy, User Dao interfaces

--- Group3Project/src/main/java/com/revature/dao/CandyDao.java
+++ Group3Project/src/main/java/com/revature/dao/CandyDao.java
@@ -1,5 +1,16 @@
 package com.revature.dao;
 
+import java.util.List;
+
+import com.revature.beans.Candy;
+
 public interface CandyDao {
+	
+	public Candy getCandyById(int id);
+	public List<Candy> getCandy();
+	public int addCandy(Candy candy);
+	public void deleteCandy(Candy candy);
+	
+	
 
 }
